335ac4e0282ba0b9b02d3ac380a821a8ea658676
Karim Hamscho <karim.hamscho@ost.ch>
2025-03-26 13:54:19 +0100

    implement upload csv and bin files

--- ActiveSense.Desktop/Views/MainWindow.axaml.cs
+++ ActiveSense.Desktop/Views/MainWindow.axaml.cs
@@ -1,3 +1,5 @@
+using System;
+using ActiveSense.Desktop.ViewModels;
 using Avalonia.Controls;
 
 namespace ActiveSense.Desktop.Views;
@@ -7,5 +9,16 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    protected override void OnDataContextChanged(EventArgs e)
+    {
+        base.OnDataContextChanged(e);
+
+        if (DataContext is MainWindowViewModel viewModel)
+        {
+            var topLevel = GetTopLevel(this);
+            if (topLevel != null) viewModel.SetStorageProvider(topLevel.StorageProvider);
+        }
     }
 }
