0089f06bf099011c0bfb9c06cc029604fa81d5b7
Karim Hamscho <karim.hamscho@ost.ch>
2025-04-01 10:03:49 +0200

    implement file picker

--- ActiveSense.Desktop/ViewModels/ProcessViewModel.cs
+++ ActiveSense.Desktop/ViewModels/ProcessViewModel.cs
@@ -1,8 +1,19 @@
+using System.Threading.Tasks;
+using ActiveSense.Desktop.Helpers;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace ActiveSense.Desktop.ViewModels;
 
 public partial class ProcessViewModel : ViewModelBase
 {
-    
+    public Interaction<string, string[]?> SelectFilesInteraction { get; } = new Interaction<string, string[]?>();
+
+    [ObservableProperty] private string[]? _selectedFiles;
+
+    [RelayCommand]
+    private async Task SelectFilesAsync()
+    {
+        SelectedFiles = await SelectFilesInteraction.HandleAsync("Hello test");
+    }
 }
--- ActiveSense.Desktop/Views/ProcessView.axaml.cs
+++ ActiveSense.Desktop/Views/ProcessView.axaml.cs
@@ -1,8 +1,12 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 using ActiveSense.Desktop.ViewModels;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Platform.Storage;
 
 namespace ActiveSense.Desktop.Views;
 
@@ -12,5 +16,40 @@ public partial class ProcessView : UserControl
     {
         InitializeComponent();
         DataContext = new ProcessViewModel();
+    }
+    
+    // Stores a reference to the disposable in order to clean it up if needed
+    IDisposable? _selectFilesInteractionDisposable;
+
+    protected override void OnDataContextChanged(EventArgs e)
+    {
+        // Dispose any old handler
+        _selectFilesInteractionDisposable?.Dispose();
+
+        if (DataContext is ProcessViewModel vm)
+        {
+            // register the interaction handler
+            _selectFilesInteractionDisposable =
+                vm.SelectFilesInteraction.RegisterHandler(InteractionHandler);
+        }
+
+        base.OnDataContextChanged(e);
+    }
+    
+    private async Task<string[]?> InteractionHandler(string input)
+    {
+        // Get a reference to our TopLevel (in our case the parent Window)
+        var topLevel = TopLevel.GetTopLevel(this);
+
+        // Try to get the files
+        var storageFiles = await topLevel!.StorageProvider.OpenFilePickerAsync(
+            new FilePickerOpenOptions()
+            {
+                AllowMultiple = true,
+                Title = input
+            });
+
+        // Transform the files as needed and return them. If no file was selected, null will be returned
+        return storageFiles?.Select(x => x.Name)?.ToArray();
     }
 }
